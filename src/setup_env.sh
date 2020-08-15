9c9527398a4341047857fb22379d7080beda263a
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-08-15 15:45:02 +0900

    hhh

--- src/setup_env.sh
+++ src/setup_env.sh
@@ -10,7 +10,7 @@ export PYTHONBIN="python"
 #export LD_LIBRARY_PATH=/anotherpath:LD_LIBRARY_PATH
 
 # Basic Theano flags
-MERLIN_THEANO_FLAGS="cuda.root=/usr/local/10.1,floatX=float32,device=cuda,on_unused_input=ignore"
+MERLIN_THEANO_FLAGS="cuda.root=/usr/local/cuda,floatX=float32,on_unused_input=ignore"
 export MERLIN_THEANO_FLAGS
 
 
